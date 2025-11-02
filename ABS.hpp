b3f12b0311393bd923f1802869e14a59b61f8e33
Kenneth <kokiidoki@gmail.com>
2025-11-02 13:02:25 -0500

    copy constructor

--- ABS.hpp
+++ ABS.hpp
@@ -11,33 +11,55 @@ template<typename T>
 class ABS : public StackInterface<T> {
 public:
     // Big 5 + Parameterized Constructor
-    ABS() = default;
-    explicit ABS(const size_t capacity): capacity_(capacity) {};
-    ABS(const ABS& other);
+    ABS() {array_ = new T[capacity_];};
+    explicit ABS(const size_t capacity): capacity_(capacity) {array_ = new T[capacity_];};
+    ABS(const ABS& other) {
+        capacity_ = other.capacity_;
+        curr_size_ = other.curr_size_;
+        array_ = new T[capacity_];
+        for (size_t i = 0; i < curr_size_; i++) {
+            array_[i] = other.array_[i];
+        }
+    }
     ABS& operator=(const ABS& rhs);
     ABS(ABS&& other) noexcept;
     ABS& operator=(ABS&& rhs) noexcept;
-    ~ABS() noexcept;
+    ~ABS() noexcept {delete[] array_;}
 
     // Get the number of items in the ABS
-    [[nodiscard]] size_t getSize() const noexcept override;
+    [[nodiscard]] size_t getSize() const noexcept override {return curr_size_;}
 
     // Get the max size of the ABS
-    [[nodiscard]] size_t getMaxCapacity() const noexcept;
+    [[nodiscard]] size_t getMaxCapacity() const noexcept {return capacity_;}
 
     // Return underlying data for the stack
-    [[nodiscard]] T* getData() const noexcept;
+    [[nodiscard]] T* getData() const noexcept {return array_;}
 
     // Push item onto the stack
-    void push(const T& data) override;
+    void push(const T& data) override {
+        if (curr_size_ == capacity_) {
+            T stack_array[curr_size_];
+            for (size_t i = 0; i < curr_size_; i++) {
+                stack_array[i] = array_[i];
+            }
+            delete[] array_;
+            capacity_ *= scale_factor_;
+            array_ = new T[capacity_];
+            for (size_t i = 0; i < curr_size_; i++) {
+                array_[i] = stack_array[i];
+            }
+        }
 
-    T peek() const override;
+        array_[curr_size_++] = data;
+    }
 
-    T pop() override;
+    T peek() const override {return array_[curr_size_ - 1];}
+
+    T pop() override {return array_[--curr_size_];}
 
 private:
     size_t capacity_ = 1;
     size_t curr_size_ = 0;
-    T* array_ = new T[capacity_];
+    T* array_;
     static constexpr size_t scale_factor_ = 2;
 };
