22ad711ef44fde6842ea61af1202b48ad08de5da
JengaDevEzE <thedevcow@gmail.com>
2025-11-04 15:40:35 -0500

    Update LinkedList

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -2,8 +2,6 @@
 #include <iostream>
 using namespace std;
 
-
-
 template <typename T>
 class LinkedList {
 public:
@@ -13,10 +11,11 @@ public:
 
 	// Accessors
 	[[nodiscard]] unsigned int getCount() const;
-	Node* getHead();
-	const Node* getHead() const;
-	Node* getTail();
-	const Node* getTail() const;
+	Node* getHead() { return head; }
+	const Node* getHead() const { return head; }
+
+	Node* getTail() { return tail; }
+	const Node* getTail() const { return tail; };
 
 	// Insertion
 	void addHead(const T& data);
@@ -38,6 +37,12 @@ public:
 	~LinkedList();
 
 private:
+	struct Node {
+		T data;
+		Node* prev;
+		Node* next;
+	};
+
 	// Stores pointers to first and last nodes and count
 	Node* head;
 	Node* tail;
@@ -45,4 +50,76 @@ private:
 
 };
 
+template <typename T>
+LinkedList<T>::LinkedList() : head(nullptr), tail(nullptr), count(0) {}
 
+template <typename T>
+LinkedList<T>::LinkedList(const LinkedList<T>& list) {
+    head = list->head;
+    tail = list->tail;
+    count = list->count;
+}
+
+template <typename T>
+LinkedList<T>::LinkedList(LinkedList<T>&& other) noexcept {
+    head = other->head;
+    tail = other->tail;
+    count = other->count;
+
+    other->head = nullptr;
+    other->tail = nullptr;
+    other->count = 0;
+}
+
+template <typename T>
+LinkedList<T>&LinkedList<T>::operator=(LinkedList<T>&& other) noexcept {
+    if (this == other) { return *this; }
+
+    Node* newHead = other->head;
+    Node* newTail = other->tail;
+    unsigned int newCount = other->count;
+    
+    delete head;
+    delete tail;
+    delete count;
+
+    head = newHead;
+    tail = newTail;
+    count = newCount;
+
+    other->head = nullptr;
+    other->tail = nullptr;
+    other->count = 0;
+
+    return *this;
+}
+
+template <typename T>
+LinkedList<T>&LinkedList<T>::operator=(const LinkedList<T>& rhs) {
+    if (this == other) { return *this; }
+    Node* newHead = other->head;
+    Node* newTail = other->tail;
+    unsigned int newCount = other->count;
+
+    delete head;
+    delete tail;
+    delete count;
+
+    head = rhs->head;
+    tail = rhs->tail;
+    count = rhs->count;
+
+    return *this;
+}
+
+template <typename T>
+LinkedList<T>::~LinkedList() {
+    delete head;
+    delete tail;
+    delete count;
+}
+
+template <typename T>
+unsigned int LinkedList<T>::getCount() const {
+    return count;
+}
