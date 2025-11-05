b3bb7201d5812653a3091f85d08c8802c6da795b
julian-perez082 <perez.julian@ufl.edu>
2025-11-05 14:22:26 -0500

    LLS complete

--- LLS.hpp
+++ LLS.hpp
@@ -11,17 +11,41 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLS();
+    LLS() {}
 
     // Insertion
-    void push(const T& item) override;
+    void push(const T& item) override {
+        list.addHead(item);
+    }
 
     // Deletion
-    T pop() override;
+    T pop() override {
+        if (list.getCount() > 0) {
+            T temp = list.head->data;
+            list.removeHead();
+            return temp;
+        }
+        throw std::out_of_range("No head found");
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override {
+        if (list.getCount() > 0) {
+            return list.head->data;
+        }
+        throw std::out_of_range("No head found");
+    }
 
     //Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return static_cast<size_t>(list.getCount());
+    }
+
+    void PrintForward() {
+        list.printForward();
+    }
+
+    void PrintReverse() {
+        list.printReverse();
+    }
 };
