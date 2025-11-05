4e7999cfb5fbe45368150a7dd56005bf29a12b78
julian-perez082 <perez.julian@ufl.edu>
2025-11-05 14:36:37 -0500

    LLDQ complete

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -15,22 +15,53 @@ private:
 
 public:
     // Constructor
-    LLDQ();
+    LLDQ() {}
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override {
+        list.addHead(item);
+    }
+    void pushBack(const T& item) override {
+        list.addTail(item);
+    }
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override {
+        if (list.getCount() > 0) {
+            T temp = list.head->data;
+            list.removeHead();
+            return temp;
+        }
+        throw std::out_of_range("No head found");
+    }
+
+    T popBack() override {
+        if (list.getCount() > 0) {
+            T temp = list.tail->data;
+            list.removeTail();
+            return temp;
+        }
+        throw std::out_of_range("No tail found");
+    }
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override {
+        if (list.getCount() > 0) {
+            return list.head->data;
+        }
+        throw std::out_of_range("No head found");
+    }
+    const T& back() const override {
+        if (list.getCount() > 0) {
+            return list.tail->data;
+        }
+        throw std::out_of_range("No tail found");
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return static_cast<size_t>(list.getCount());
+    }
 };
 
 
