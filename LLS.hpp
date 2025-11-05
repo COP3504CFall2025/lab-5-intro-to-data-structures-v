c42dfe93a14b3398ba8194076e3f69e0419f19da
Kenneth <kokiidoki@gmail.com>
2025-11-05 15:46:29 -0500

    linkedlist nodes are already on heap

--- LLS.hpp
+++ LLS.hpp
@@ -8,7 +8,7 @@
 template <typename T>
 class LLS : public StackInterface<T> {
 private:
-    LinkedList<T> list = new LinkedList<T>;
+    LinkedList<T> list = LinkedList<T>;
 public:
     // Constructor
     LLS() = default;
