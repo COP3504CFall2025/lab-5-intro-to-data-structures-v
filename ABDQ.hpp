57ae01ae23ac660bc48063413393312952ad13d5
kushp-beep <kushp-beep@github.com>
2025-11-09 18:00:23 -0500

    fixed

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -128,6 +128,7 @@ public:
             data_[i] = data_[i + 1];
         }
         size_--;
+        front_ = 0;
         if (capacity_ / SCALE_FACTOR > size_) {
             shrinkIfNeeded();
         }
@@ -139,6 +140,7 @@ public:
         }
         T value = data_[back_];
         size_--;
+        back_ = size_ - 1;
         if (capacity_ / SCALE_FACTOR > size_) {
             shrinkIfNeeded();
         }
