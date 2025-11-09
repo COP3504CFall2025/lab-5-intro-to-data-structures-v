0253a2b6da52816c15069d975d5b4073a587a7c4
Julian <jdperez0828@gmail.com>
2025-11-08 19:45:51 -0500

    Test

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -6,19 +6,21 @@
 #include <utility>
 
 template <typename T>
-class ABDQ : public DequeInterface<T> {
+class ABDQ : public DequeInterface<T>
+{
 private:
-    T* data_;                 // underlying dynamic array
-    std::size_t capacity_;    // total allocated capacity
-    std::size_t size_;        // number of stored elements
-    std::size_t front_;       // index of front element
-    std::size_t back_;        // index after the last element (circular)
+    T *data_;              // underlying dynamic array
+    std::size_t capacity_; // total allocated capacity
+    std::size_t size_;     // number of stored elements
+    std::size_t front_;    // index of front element
+    std::size_t back_;     // index after the last element (circular)
 
     static constexpr std::size_t SCALE_FACTOR = 2;
 
 public:
     // Big 5
-    ABDQ() {
+    ABDQ()
+    {
         this->capacity_ = 4;
         this->size_ = 0;
         this->front_ = 0;
@@ -26,7 +28,8 @@ public:
         this->data_ = new T[this->capacity_];
     }
 
-    explicit ABDQ(std::size_t capacity) {
+    explicit ABDQ(std::size_t capacity)
+    {
         this->capacity_ = capacity;
         this->size_ = 0;
         this->front_ = 0;
@@ -34,19 +37,22 @@ public:
         this->data_ = new T[this->capacity_];
     }
 
-    ABDQ(const ABDQ& other) {
+    ABDQ(const ABDQ &other)
+    {
         this->capacity_ = other.capacity_;
         this->size_ = other.size_;
         this->front_ = other.front_;
         this->back_ = other.back_;
         this->data_ = new T[this->capacity_];
 
-        for (size_t i = 0; i < this->size_; i++) {
+        for (size_t i = 0; i < this->size_; i++)
+        {
             this->data_[i] = other.data_[i];
         }
     }
 
-    ABDQ(ABDQ&& other) noexcept {
+    ABDQ(ABDQ &&other) noexcept
+    {
         this->capacity_ = other.capacity_;
         this->size_ = other.size_;
         this->front_ = other.front_;
@@ -60,12 +66,14 @@ public:
         other.data_ = nullptr;
     }
 
-    ABDQ& operator=(const ABDQ& other) {
-        if (this == &other) {
+    ABDQ &operator=(const ABDQ &other)
+    {
+        if (this == &other)
+        {
             return *this;
         }
-        
-        T* temp = new T[other.capacity_];
+
+        T *temp = new T[other.capacity_];
         delete[] this->data_;
         this->data_ = temp;
         this->capacity_ = other.capacity_;
@@ -73,12 +81,15 @@ public:
         this->front_ = other.front_;
         this->back_ = other.back_;
 
-        for (size_t i = 0; i < size_; i++) {
+        for (size_t i = 0; i < size_; i++)
+        {
             this->data_[i] = other.data_[i];
         }
     }
-    ABDQ& operator=(ABDQ&& other) noexcept {
-        if (this == &other) {
+    ABDQ &operator=(ABDQ &&other) noexcept
+    {
+        if (this == &other)
+        {
             return *this;
         }
 
@@ -96,7 +107,8 @@ public:
         other.data_ = nullptr;
     }
 
-    ~ABDQ() override {
+    ~ABDQ() override
+    {
         delete[] this->data_;
         this->data_ = nullptr;
         this->capacity_ = 0;
@@ -106,20 +118,38 @@ public:
     }
 
     // Insertion
-    void pushFront(const T& item) override {
-        if (size_ > capacity)
+    void pushFront(const T &item) override
+    {
+        if (size_ > capacity_)
+        {
+            //     resize();
+        }
+
+        // this->data_[front_ - 1] = item;
+        // --front_;
+        // ++size_;
     }
-    void pushBack(const T& item) override;
+
+    void pushBack(const T &item) override
+    {
+    }
+
+    /*void resize()
+    {
+        T *temp = new T[capacity_ * 2];
+        this->capacity_ *= 2;
+
+        for (size)
+    }*/
 
     // Deletion
     T popFront() override;
     T popBack() override;
 
     // Access
-    const T& front() const override;
-    const T& back() const override;
+    const T &front() const override;
+    const T &back() const override;
 
     // Getters
     std::size_t getSize() const noexcept override;
-
 };
--- LinkedList.hpp
+++ LinkedList.hpp
@@ -3,58 +3,68 @@
 using namespace std;
 
 template <typename T>
-struct Node {
+struct Node
+{
     T data;
-    Node<T>* prev;
-    Node<T>* next;
+    Node<T> *prev;
+    Node<T> *next;
 };
 
 template <typename T>
-class LinkedList {
+class LinkedList
+{
 public:
-	// Behaviors
-	void printForward() const {
-        if (!head) {
+    // Behaviors
+    void printForward() const
+    {
+        if (!head)
+        {
             return;
         }
 
-        Node<T>* current = head;
+        Node<T> *current = head;
 
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++)
+        {
             std::cout << current->data << std::endl;
             current = current->next;
         }
     }
-	void printReverse() const {
-        if (!tail) {
+    void printReverse() const
+    {
+        if (!tail)
+        {
             return;
         }
 
-        Node<T>* current = tail;
+        Node<T> *current = tail;
 
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++)
+        {
             std::cout << current->data << std::endl;
             current = current->prev;
         }
     }
 
-	// Accessors
-	[[nodiscard]] unsigned int getCount() const { return count; }
-	Node<T>* getHead() { return head; }
-	const Node<T>* getHead() const { return head; }
+    // Accessors
+    [[nodiscard]] unsigned int getCount() const { return count; }
+    Node<T> *getHead() { return head; }
+    const Node<T> *getHead() const { return head; }
 
-	Node<T>* getTail() { return tail; }
-	const Node<T>* getTail() const { return tail; }
+    Node<T> *getTail() { return tail; }
+    const Node<T> *getTail() const { return tail; }
 
-	// Insertion
-	void addHead(const T& data) {
-        Node<T>* newNode = new Node<T>;
-        
+    // Insertion
+    void addHead(const T &data)
+    {
+        Node<T> *newNode = new Node<T>;
+
         newNode->data = data;
         newNode->prev = nullptr;
         newNode->next = head;
 
-        if (head != nullptr) {
+        if (head != nullptr)
+        {
             head->prev = newNode;
         }
 
@@ -62,14 +72,16 @@ public:
         count++;
     }
 
-	void addTail(const T& data) {
-        Node<T>* newNode = new Node<T>;
+    void addTail(const T &data)
+    {
+        Node<T> *newNode = new Node<T>;
 
         newNode->data = data;
         newNode->prev = tail;
         newNode->next = nullptr;
 
-        if (tail != nullptr) {
+        if (tail != nullptr)
+        {
             tail->next = newNode;
         }
 
@@ -77,17 +89,21 @@ public:
         count++;
     }
 
-	// Removal
-	bool removeHead() {
-        if (head != nullptr) {
-            Node<T>* temp = head;
+    // Removal
+    bool removeHead()
+    {
+        if (head != nullptr)
+        {
+            Node<T> *temp = head;
 
             head = head->next;
-            
-            if (head != nullptr) {
+
+            if (head != nullptr)
+            {
                 head->prev = nullptr;
             }
-            else {
+            else
+            {
                 tail = nullptr;
             }
 
@@ -102,11 +118,14 @@ public:
         }
         return false;
     }
-	bool removeTail() {
-        if (tail != nullptr) {
-            Node<T>* temp = tail;
+    bool removeTail()
+    {
+        if (tail != nullptr)
+        {
+            Node<T> *temp = tail;
 
-            if (tail->prev != nullptr) {
+            if (tail->prev != nullptr)
+            {
                 tail->prev->next = nullptr;
                 tail = tail->prev;
             }
@@ -120,47 +139,49 @@ public:
 
             return true;
         }
-        return false;        
+        return false;
     }
-	void Clear() {
-        while (head != nullptr) {
+    void clear()
+    {
+        while (head != nullptr)
+        {
             removeHead();
         }
 
         count = 0;
     }
 
-	// Operators
-	LinkedList<T>& operator=(LinkedList<T>&& other) noexcept;
-	LinkedList<T>& operator=(const LinkedList<T>& rhs);
+    // Operators
+    LinkedList<T> &operator=(LinkedList<T> &&other) noexcept;
+    LinkedList<T> &operator=(const LinkedList<T> &rhs);
 
-	// Construction/Destruction
-	LinkedList();
-	LinkedList(const LinkedList<T>& list);
-	LinkedList(LinkedList<T>&& other) noexcept;
-	~LinkedList();
+    // Construction/Destruction
+    LinkedList();
+    LinkedList(const LinkedList<T> &list);
+    LinkedList(LinkedList<T> &&other) noexcept;
+    ~LinkedList();
 
 private:
-
-	// Stores pointers to first and last Node<T>s and count
-	Node<T>* head;
-	Node<T>* tail;
-	unsigned int count;
-
+    // Stores pointers to first and last Node<T>s and count
+    Node<T> *head;
+    Node<T> *tail;
+    unsigned int count;
 };
 
 template <typename T>
 LinkedList<T>::LinkedList() : head(nullptr), tail(nullptr), count(0) {}
 
 template <typename T>
-LinkedList<T>::LinkedList(const LinkedList<T>& list) {
+LinkedList<T>::LinkedList(const LinkedList<T> &list)
+{
     head = list->head;
     tail = list->tail;
     count = list->count;
 }
 
 template <typename T>
-LinkedList<T>::LinkedList(LinkedList<T>&& other) noexcept {
+LinkedList<T>::LinkedList(LinkedList<T> &&other) noexcept
+{
     head = other->head;
     tail = other->tail;
     count = other->count;
@@ -171,13 +192,17 @@ LinkedList<T>::LinkedList(LinkedList<T>&& other) noexcept {
 }
 
 template <typename T>
-LinkedList<T>&LinkedList<T>::operator=(LinkedList<T>&& other) noexcept {
-    if (this == &other) { return *this; }
+LinkedList<T> &LinkedList<T>::operator=(LinkedList<T> &&other) noexcept
+{
+    if (this == &other)
+    {
+        return *this;
+    }
 
-    Node<T>* newHead = other->head;
-    Node<T>* newTail = other->tail;
+    Node<T> *newHead = other->head;
+    Node<T> *newTail = other->tail;
     unsigned int newCount = other->count;
-    
+
     delete head;
     delete tail;
     delete count;
@@ -194,10 +219,14 @@ LinkedList<T>&LinkedList<T>::operator=(LinkedList<T>&& other) noexcept {
 }
 
 template <typename T>
-LinkedList<T>&LinkedList<T>::operator=(const LinkedList<T>& rhs) {
-    if (this == &rhs) { return *this; }
-    Node<T>* newHead = rhs->head;
-    Node<T>* newTail = rhs->tail;
+LinkedList<T> &LinkedList<T>::operator=(const LinkedList<T> &rhs)
+{
+    if (this == &rhs)
+    {
+        return *this;
+    }
+    Node<T> *newHead = rhs->head;
+    Node<T> *newTail = rhs->tail;
     unsigned int newCount = rhs->count;
 
     delete head;
@@ -212,7 +241,8 @@ LinkedList<T>&LinkedList<T>::operator=(const LinkedList<T>& rhs) {
 }
 
 template <typename T>
-LinkedList<T>::~LinkedList() {
+LinkedList<T>::~LinkedList()
+{
     delete head;
     delete tail;
     delete count;
