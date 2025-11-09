285cea7e51aa4f2be70940f1736ffc2f1c499ed1
kushp-beep <kushp-beep@github.com>
2025-11-09 17:03:48 -0500

    lldq

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -15,22 +15,60 @@ private:
 
 public:
     // Constructor
-    LLDQ();
+    LLDQ() : list() {};
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override {
+        list.addHead(item);
+    };
+    void pushBack(const T& item) override {
+        list.addTail(item);
+    };
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override {
+        if (list.getCount() == 0) {
+            throw std::runtime_error("LLS::front() called on an empty LLS");
+        }
+        T value = list.getHead()->data;
+        list.removeHead();
+        return value;
+    };
+    T popBack() override {
+        if (list.getCount() == 0) {
+            throw std::runtime_error("LLS::front() called on an empty LLS");
+        }
+        T value = list.getTail()->data;
+        list.removeTail();
+        return value;
+    };
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override {
+        if (list.getCount() == 0) {
+            throw std::runtime_error("LLS::front() called on an empty LLS");
+        }
+        return list.getHead()->data;
+    };
+    const T& back() const override {
+        if (list.getCount() == 0) {
+            throw std::runtime_error("LLS::back() called on an empty LLS");
+        }
+        return list.getTail()->data;
+    };
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        list.getCount();
+    };
+
+    void printForward() {
+        list.printForward();
+    }
+
+    void printReverse() {
+        list.printReverse();
+    }
 };
 
 
